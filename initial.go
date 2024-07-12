5feeb37bdee4e675f88b10167d257387d9adc994
anishsharma21 <anisharma2103@gmail.com>
2024-07-12 23:17:04 +1000

    created a little web server that returns hello world when accessed - pretty neat. so cool how simple it is to do something like this in Go

--- initial.go
+++ initial.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", helloWorldHandler)
+	fmt.Println("Server starting on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
 }
