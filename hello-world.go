ce8712d728b2aa13b312215ebeea3c414929455a
anishsharma21 <anisharma2103@gmail.com>
2024-07-13 23:23:58 +1000

    function declarations

--- hello-world.go
+++ hello-world.go
@@ -1,11 +1,11 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"rsc.io/quote"
-)
+func add(x int, y int) int {
+	return x + y
+}
 
 func main() {
-	fmt.Println(quote.Go())
+	fmt.Println(add(3, 4))
 }
