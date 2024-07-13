48c0f3606f84cbb1868f407162ec1dd811f60d8e
anishsharma21 <anisharma2103@gmail.com>
2024-07-13 23:34:19 +1000

    further exploration of variable declarations and Go specific semantics and syntax:

--- hello-world.go
+++ hello-world.go
@@ -2,10 +2,22 @@ package main
 
 import "fmt"
 
-func add(x int, y int) int {
-	return x + y
+var c, java, python = "no", true, false
+
+func swap(x, y string) (string, string) {
+	return y, x
+	
+}
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
 
 func main() {
-	fmt.Println(add(3, 4))
+	var i, j int = 1, 2
+	a, b := swap("there", "hi")
+	fmt.Println(a, b)
+	fmt.Println(split(18))
+	fmt.Println(i, j, c, java, python)
 }
